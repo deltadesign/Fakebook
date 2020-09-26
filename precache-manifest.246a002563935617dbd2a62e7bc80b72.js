92b73cfde211951be53e73dece64d2a3a6ae6017
Dan Thomas <66531548+deltadesign@users.noreply.github.com>
2020-09-26 07:14:33 +0100

    Updates

---
+++ precache-manifest.246a002563935617dbd2a62e7bc80b72.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99a32211ec7296b04b83f9f9f115c613",
+    "url": "/Fakebook/index.html"
+  },
+  {
+    "revision": "dfc2198913ae212ff5f6",
+    "url": "/Fakebook/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "d485b93bb040058405b7",
+    "url": "/Fakebook/static/css/main.bcebed1f.chunk.css"
+  },
+  {
+    "revision": "dfc2198913ae212ff5f6",
+    "url": "/Fakebook/static/js/2.f5ee90f7.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Fakebook/static/js/2.f5ee90f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d485b93bb040058405b7",
+    "url": "/Fakebook/static/js/main.f7955910.chunk.js"
+  },
+  {
+    "revision": "1f1d31160c25b41e54bd",
+    "url": "/Fakebook/static/js/runtime-main.979be159.js"
+  }
+]);
